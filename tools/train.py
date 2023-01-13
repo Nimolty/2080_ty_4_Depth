9462ade8148fc13de233dc08a79293232d6bc672
Yangtian <1900010617@pku.edu.cn>
2023-01-14 00:41:30 +0800

    0114

--- tools/train.py
+++ tools/train.py
@@ -5,6 +5,8 @@ import time
 import multiprocessing as mp
 import os
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 from tabulate import tabulate
 from tqdm import tqdm
 from torch.utils.data import DataLoader
@@ -18,11 +20,13 @@ from torch.nn import functional as F
 
 from depth_c2rp.models.heads import FaPNHead
 from depth_c2rp.models.backbones import ResNet
-from depth_c2rp.optimizers import get_optimizer
+from depth_c2rp.optimizers import get_optimizer, adapt_lr
 from depth_c2rp.build import build_model
-from depth_c2rp.losses import get_loss
-from depth_c2rp.utils.utils import save_model, load_model, exists_or_mkdir, visualize_training_loss
+from depth_c2rp.losses import get_loss, Calculate_Loss
+from depth_c2rp.utils.utils import save_model, load_model, exists_or_mkdir, visualize_training_loss, find_seq_data_in_dir
 from depth_c2rp.configs.config import update_config
+from depth_c2rp.datasets.datasets import Depth_dataset
+from inference import network_inference
 
 
 def main(cfg):
@@ -34,16 +38,24 @@ def main(cfg):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu' )
     
     # Build DataLoader
-    
+    dataset_cfg, train_cfg = cfg["DATASET"], cfg["TRAIN"]
+    training_data_dir = dataset_cfg["TRAINING_ROOT"]
+    training_data = find_seq_data_in_dir(training_data_dir)
+    training_dataset = Depth_dataset(training_data, dataset_cfg["MANIPULATOR"], dataset_cfg["KEYPOINT_NAMES"], dataset_cfg["JOINT_NAMES"], \
+                                     dataset_cfg["INPUT_RESOLUTION"], mask_dict=dataset_cfg["MASK_DICT"])
+    training_loader = torch.utils.data.DataLoader(training_dataset, batch_size=train_cfg["BATCH_SIZE"], shuffle=True, \
+                                               num_workers=train_cfg["NUM_WORKERS"], pin_memory=True, drop_last=True)
     
     # Build Model
     model_cfg = cfg["MODEL"]
-    model = build_model(model_cfg["BACKBONE"], model_cfg["HEAD"], model_cfg["MODEL_CLASSES"], model_cfg["IN_CHANNELS"])
+    model = build_model(model_cfg["BACKBONE"], model_cfg["HEAD"], model_cfg["MODEL_CLASSES"], model_cfg["IN_CHANNELS"], \
+                        dataset_cfg["NUM_JOINTS"], dataset_cfg["OUTPUT_RESOLUTION"][0], dataset_cfg["OUTPUT_RESOLUTION"][1])
     model.init_pretrained(model_cfg["PRETRAINED"])
     
     # Build Loss Function
     loss_cfg = cfg["LOSS"]
-    loss_fn = get_loss(loss_cfg["NAME"], int(loss_cfg["IGNORE_LABELS"]))
+    loss_fn_names = {"masks" : loss_cfg["NAME"]}
+    loss_fn = Calculate_Loss(loss_fn_names,weights=loss_cfg["WEIGHTS"])
     
     # Build Optimizer
     optim_cfg = cfg["OPTIMIZER"]
@@ -70,27 +82,55 @@ def main(cfg):
         
     
     # Training 
-    model.train()
-    model = model.to(device)
-    train_cfg = cfg["TRAIN"]
+    device_ids = [6, 7] 	
+    model = torch.nn.DataParallel(model, device_ids=device_ids)
+    device = torch.device('cuda:{}'.format(device_ids[0]) if torch.cuda.is_available() else 'cpu' )
+    model = model.to(device)    
+    max_iters = len(training_loader) * train_cfg["EPOCHS"]
+    print("len_dataloader", len(training_loader))
+    
     for epoch in tqdm(range(start_epoch + 1, train_cfg["EPOCHS"] + 1)):
-        start_time = time.time()
-        x = torch.randn(1, 3, 720, 1280).to(device)
-        label = torch.ones((1,720, 1280), dtype=torch.long).to(device)
-        middle_time = time.time() 
+        model.train()
+        for batch_idx, batch in tqdm(enumerate(training_loader)):
+            adapt_lr(optimizer, epoch, batch_idx, len(training_loader), optim_cfg["LR"], max_iters)
+            start_time = time.time()
+#            if batch_idx > 1:
+#                break
+            
+            next_img = batch["next_frame_img_as_input"].to(device)
+            next_xy_wrt_cam, next_uv, next_simdepth, next_normals = batch["next_frame_xy_wrt_cam"].to(device), batch["next_frame_uv"].to(device), \
+            batch["next_frame_simdepth_as_input"].to(device), batch["next_normals_crop"].to(device)
+            next_input = torch.cat([next_img, next_simdepth, next_xy_wrt_cam, next_uv, next_normals], dim=1)
+            
+            batch_gt_masks, batch_gt_joints_pos, batch_gt_joints_wrt_cam, batch_gt_joints_wrt_rob = batch["next_frame_mask_as_input"].to(device), \
+            batch["next_frame_joints_pos"].to(device), batch["next_frame_joints_wrt_cam"].to(device), batch["next_frame_joints_wrt_rob"].to(device)
+            middle_time = time.time()
+            mask_out, trans_out, quat_out, joint_3d, joint_pos = model(next_input)
+            end_time = time.time()
+            
+#            print("Image Construction Time", middle_time - start_time)
+#            print("Model Time", end_time - middle_time)
+            
+            losses = loss_fn(trans_out, quat_out, joint_pos,joint_3d, mask_out, batch_gt_masks, batch_gt_joints_pos, batch_gt_joints_wrt_cam, batch_gt_joints_wrt_rob)
+            
+            optimizer.zero_grad()
+            losses["total_loss"].backward()
+            optimizer.step()
+            
+            if batch_idx % 10 == 0:
+                print("Total Loss : \n", losses["total_loss"].item())
+                print("Mask Loss : \n", losses["mask_loss"].item())
+                print("Joint 3d Loss : \n", losses["joint_3d_loss"].item())
+                print("Joint Pos Loss : \n", losses["joint_pos_loss"].item())
+                print("RT Loss : \n", losses["rt_loss"].item())
+                
+            if batch_idx % 30 == 0:
+                visualize_training_loss(losses, writer, batch_idx, epoch, len(training_loader))
         
-        out = model(x)
-        end_time = time.time()
         
-        print("Image Construction Time", middle_time - start_time)
-        print("Model Time", end_time - middle_time)
+        # Inference 
+        network_inference(model, cfg, epoch, device)
         
-        loss = loss_fn(out, label)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-
         # Visualization
     
     
@@ -103,6 +143,7 @@ def main(cfg):
 if __name__ == "__main__":
     cfg, args = update_config()
     main(cfg)
+#    find_seq_data_in_dir("/DATA/disk1/hyperplane/Depth_C2RP/Data/TY/")
 
 
 
